cf81777b98e3a32c500aec1c3f93af842e90a90e
Chris Schwerdt <chris.schwerdt@pinnacleqi.com>
2017-09-21 15:57:55 -0600

    Initial commit with implementation of an EmailAddress value object

---
+++ src/EmailAddress.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Pinnacle\CommonValueObjects;
+
+use InvalidArgumentException;
+
+class EmailAddress
+{
+    /**
+     * @var string
+     */
+    private $emailAddressString;
+
+    /**
+     * EmailAddress constructor.
+     *
+     * @param string $emailAddressString
+     *
+     * @throws InvalidArgumentException
+     */
+    public function __construct(string $emailAddressString)
+    {
+        if (!filter_var($emailAddressString, FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidArgumentException('The specified value does not appear to be a valid email address.');
+        }
+
+        $this->emailAddressString = $emailAddressString;
+    }
+
+    /**
+     * Returns the string representation of an email address.
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->emailAddressString;
+    }
+
+    /**
+     * Returns the email address as a string.
+     *
+     * @return string
+     */
+    public function value(): string
+    {
+        return $this->emailAddressString;
+    }
+
+    /**
+     * Returns the portion of the email address before the "@" sign.
+     *
+     * @return string
+     */
+    public function localPart(): string
+    {
+        return substr($this->emailAddressString, 0, strrpos($this->emailAddressString, '@'));
+    }
+
+    /**
+     * Returns the portion of the email address after the "@" sign.
+     *
+     * @return string
+     */
+    public function domainPart(): string
+    {
+        return substr($this->emailAddressString, strrpos($this->emailAddressString, '@') + 1);
+    }
+
+    /**
+     * Attempts to parse the specified email address.
+     *
+     * @param string       $emailAddressString The email address string to try parsing.
+     * @param EmailAddress $emailAddress       The variable to assign the email address to.
+     *
+     * @return bool Whether the specified email address could be parsed.
+     */
+    public static function tryParse(string $emailAddressString, &$emailAddress)
+    {
+        try {
+            $emailAddress = new static($emailAddressString);
+
+            return true;
+        } catch (InvalidArgumentException $e) {
+            $emailAddress = null;
+
+            return false;
+        }
+    }
+}
---
+++ tests/EmailAddressTest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Pinnacle\CommonValueObjects\Tests;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Pinnacle\CommonValueObjects\EmailAddress;
+
+class EmailAddressTest extends TestCase
+{
+    /**
+     * @param string $invalidEmailAddressString
+     *
+     * @dataProvider invalidEmailAddressStrings
+     */
+    public function testInvalidAddresses(string $invalidEmailAddressString)
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new EmailAddress($invalidEmailAddressString);
+    }
+
+    /**
+     * @param string $invalidEmailAddressString
+     *
+     * @dataProvider invalidEmailAddressStrings
+     */
+    public function testTryParseInvalidAddresses(string $invalidEmailAddressString)
+    {
+        $this->assertFalse(EmailAddress::tryParse($invalidEmailAddressString, $emailAddress));
+    }
+
+    /**
+     * @param EmailAddress $emailAddress
+     *
+     * @dataProvider validEmailAddressesWithParts
+     */
+    public function testTryParseValidAddresses(EmailAddress $emailAddress)
+    {
+        /* @var EmailAddress $result */
+        $this->assertTrue(EmailAddress::tryParse($emailAddress->value(), $result));
+        $this->assertSame($emailAddress->value(), $result->value());
+    }
+
+    /**
+     * @param EmailAddress $emailAddress
+     * @param string       $localPart
+     *
+     * @dataProvider validEmailAddressesWithParts
+     */
+    public function testLocalPart(EmailAddress $emailAddress, string $localPart)
+    {
+        $this->assertSame($localPart, $emailAddress->localPart());
+    }
+
+    /**
+     * @param EmailAddress $emailAddress
+     * @param string       $localPart
+     * @param string       $domainPart
+     *
+     * @dataProvider validEmailAddressesWithParts
+     */
+    public function testDomainPart(EmailAddress $emailAddress, string $localPart, string $domainPart)
+    {
+        $this->assertSame($domainPart, $emailAddress->domainPart());
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function invalidEmailAddressStrings(): array
+    {
+        return [
+            [' fake@example.com'],
+            ['fake@example.com '],
+            ['asdf'],
+            [' fake@example.com '],
+            ['localhost'],
+            ['bad@localhost'],
+            ['bad@domain'],
+            ['bad@bad.com@bad'],
+        ];
+    }
+
+    /**
+     * @return EmailAddress[][]
+     */
+    public function validEmailAddressesWithParts(): array
+    {
+        return [
+            [new EmailAddress('test@example.com'), 'test', 'example.com'],
+            [new EmailAddress('jimmy.lee@sub.example.com'), 'jimmy.lee', 'sub.example.com'],
+            [new EmailAddress('j@j.com'), 'j', 'j.com'],
+            [
+                new EmailAddress('disposable.style.email.with+symbol@example.com'),
+                'disposable.style.email.with+symbol',
+                'example.com',
+            ],
+            [
+                new EmailAddress('"very.unusual.@.unusual.com"@example.com'),
+                '"very.unusual.@.unusual.com"',
+                'example.com',
+            ],
+            [
+                new EmailAddress('customer/department=shipping@example.com'),
+                'customer/department=shipping',
+                'example.com',
+            ],
+        ];
+    }
+}
